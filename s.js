a961c3383c888d06509062aca6333d74d2cda45c
millerdamian520 <155296415+millerdamian520@users.noreply.github.com>
2024-02-25 07:33:17 +0100

    Add files via upload

---
+++ s.js
@@ -0,0 +1,20 @@
+function verif()
+{
+    {
+if("this nom<8")
+return false;
+    }
+{
+if ("this prenom>8")
+returnn false;
+}
+{
+    if("this email")
+    return false;
+}
+{
+    if ("this telephone")
+    return false;
+}
+
+}
